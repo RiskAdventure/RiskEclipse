aef9c6fa64042c9b8b5b0b5107f293e83f452abc
Lidia <lidiacapita@gmail.com>
2021-05-19 20:43:07 +0200

    Modificacion Eventos Dao e Int

---
+++ TFG_RiskAdventure_SPRING/src/main/java/com/ite/riskadventureSPRING/modelo/dao/EventoDaoImpl.java
@@ -0,0 +1,102 @@
+package com.ite.riskadventureSPRING.modelo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ite.riskadventureSPRING.modelo.beans.Evento;
+import com.ite.riskadventureSPRING.modelo.repository.EventoRepository;
+
+
+public class EventoDaoImpl implements IntEventoDao{
+	
+	@Autowired
+	EventoRepository evrepo;
+
+	@Override
+	public List<Evento> verTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Evento> buscarEventosActivos(String estado) {
+		try {
+		return evrepo.findByEstado(estado);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return new ArrayList<Evento>();
+		}
+	}
+
+	@Override
+	public int crearEvento(Evento evento) {
+		//Creamos variable filas = 0
+		int filas = 0;
+		
+		//Si el try va bien, filas pasa a ser 1
+		try {
+			evrepo.save(evento);
+			filas = 1;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		//Devolvemos valor de filas
+		return filas;
+	}
+
+	@Override
+	public Evento mostrarEvento(int idEvento) {
+		//Si existe, devuelve el evento. Sino, devuelve nulo
+		return evrepo.findById(idEvento).orElse(null);
+	}
+
+	@Override
+	public int eliminarEvento(int idEvento) {
+		int filas = 0;
+		
+		try {
+			evrepo.deleteById(idEvento);
+			filas = 1;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return filas;
+	}
+
+	@Override
+	public int cancelarEvento(int idEvento) {
+		int filas = 0;
+		
+		Evento eventoCanc = evrepo.findById(idEvento).get();
+		eventoCanc.setEstado("cancelado");
+		
+		try {
+			evrepo.save(eventoCanc);
+			filas = 1;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return filas;
+	}
+
+	@Override
+	public List<Evento> mostrarDestacados(String destacado, String estado) {
+		return evrepo.findByDestacadoAndEstado(destacado, estado);
+	}
+
+	@Override
+	public List<Evento> mostrarEventoPorTipo(int idTipo) {
+		
+		if(idTipo == -1) {
+			return evrepo.findAll();
+		} else {
+			return evrepo.findByTipo(idTipo);
+		}
+	}
+}
---
+++ TFG_RiskAdventure_SPRING/src/main/java/com/ite/riskadventureSPRING/modelo/dao/IntEventoDao.java
@@ -0,0 +1,18 @@
+package com.ite.riskadventureSPRING.modelo.dao;
+
+import java.util.List;
+
+import com.ite.riskadventureSPRING.modelo.beans.Evento;
+
+public interface IntEventoDao {
+	
+	List<Evento> verTodos();
+	List<Evento> buscarEventosActivos(String estado);
+	int crearEvento(Evento evento);
+	Evento mostrarEvento(int idEvento);
+	int eliminarEvento(int idEvento);
+	int cancelarEvento(int idEvento);
+	List<Evento> mostrarDestacados(String destacado, String activo);
+	List<Evento> mostrarEventoPorTipo(int idTipo);
+
+}
